03773c7626689e9368f29a047a501f069fb2043e
jamesscaggs <james.scaggs@gmail.com>
2020-10-02 14:39:50 -0500

    clean up

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 // SERVER
