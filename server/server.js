0629064546d2b929e849c283571f15da765ce33f
jamesscaggs <james.scaggs@gmail.com>
2020-10-02 14:46:01 -0500

    fixed webpack

--- server/server.js
+++ server/server.js
@@ -11,18 +11,22 @@ const PORT = process.env.PORT || 3000;
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
-// ERROR HANDLER
-server.use(() => {
-  const defaultErr = {
-    log: 'Express error: Unknown middleware',
-    status: 500,
-    message: { err: 'An error occurred' },
-  };
-  const errorObj = { ...defaultErr, err };
-
-  console.log('ERROR LOG => ', errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'hello' });
 });
+
+// ERROR HANDLER
+// server.use(() => {
+//   const defaultErr = {
+//     log: 'Express error: Unknown middleware',
+//     status: 500,
+//     message: { err: 'An error occurred' },
+//   };
+//   const errorObj = { ...defaultErr, err };
+
+//   console.log('ERROR LOG => ', errorObj.log);
+//   return res.status(errorObj.status).json(errorObj.message);
+// });
 
 // statically serve everything in the build folder on the route '/build'
 if (process.env.NODE_ENV === 'production') {
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     compress: true,
     port: 8080,
     proxy: {
-      '/': 'http://localhost:3000',
+      '/api': 'http://localhost:3000',
     },
   },
   output: {
