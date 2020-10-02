c215f652b1c52476b7ac7abe5b11ce59b3e45773
jamesscaggs <james.scaggs@gmail.com>
2020-10-02 14:35:37 -0500

    scaffold

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// SERVER
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+// SET UP
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
+// ERROR HANDLER
+server.use(() => {
+  const defaultErr = {
+    log: 'Express error: Unknown middleware',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = { ...defaultErr, err };
+
+  console.log('ERROR LOG => ', errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+// statically serve everything in the build folder on the route '/build'
+if (process.env.NODE_ENV === 'production') {
+  server.use('/build', express.static(path.join(__dirname, '../build')));
+  // serve index.html on the route '/'
+  server.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+  });
+}
+
+server.listen(PORT);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'production',
+  entry: './client/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    publicPath: 'http://localhost:8080/build/',
+    compress: true,
+    port: 8080,
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s(a|c)ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
