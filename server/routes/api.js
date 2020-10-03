ed42ff3c64d9ff138f68b46984512b656d855e1f
jamesscaggs <james.scaggs@gmail.com>
2020-10-03 11:46:47 -0500

    set up webhook endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -11,4 +11,12 @@ router.get('/', (req, res) => {
 // THIS IS THE MAIN CALLBACK URL FOR GITHUB OAUTH
 router.get('/oauth/callback', userController.authenticateUser, userController.requestToken);
 
+// GITHUB APP CALLBACK
+router.get('/github_app/callback', userController.authenticateUser, userController.requestToken);
+
+// GITHUB WEBHOOK
+router.get('/github/webhook', (req, res) => {
+  console.log({ req });
+  res.send(200).json({ message: 'pong' });
+});
 module.exports = router;
