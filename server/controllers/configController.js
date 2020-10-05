3d6340021bedf286bea6ea65f1127df23f4cefca
jamesscaggs <james.scaggs@gmail.com>
2020-10-05 15:12:01 -0500

    init

--- server/controllers/configController.js
+++
@@ -1,6 +0,0 @@
-const Session = require('../models/sessionModels');
-const User = require('../models/userModels');
-const Config = require('../models/configModels');
-const axios = require('axios');
-
-const configController = {};
